f1e034e48a9f6095e5464914d787f5d46d6eaa9b
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2023-02-19 16:47:40 +0500

    Query Client removeQueries method

--- lazy-days/client/src/components/user/hooks/useUser.ts
+++ lazy-days/client/src/components/user/hooks/useUser.ts
@@ -48,6 +48,7 @@ export function useUser(): UseUser {
   // meant to be called from useAuth
   function clearUser() {
     queryClient.removeQueries(queryKeys.user, null);
+    queryClient.removeQueries('user-appointments');
   }
 
   return { user, updateUser, clearUser };
