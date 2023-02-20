59a4bbdc7eb80bd5d4a23277018cf3cee68c509b
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2023-02-21 00:57:00 +0500

    Testing Rendered Query Data

--- lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
+++ lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
@@ -3,6 +3,12 @@ import { renderWithQueryClient } from 'test-utils';
 
 import { Treatments } from '../Treatments';
 
-test('renders response from query', () => {
+test('renders response from query', async () => {
     renderWithQueryClient(<Treatments />);
+
+    const treatments = await screen.findAllByRole('heading' ,{
+      name :/massage|facial|scrub/i,
+    });
+
+    expect(treatments).toHaveLength(3);
 });
