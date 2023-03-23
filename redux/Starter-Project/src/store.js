2a9a73c4eab79c0dfd98acf7123de8d00dd54ed2
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2023-03-23 23:46:18 +0500

    Created Store

--- redux/Starter-Project/src/store.js
+++ redux/Starter-Project/src/store.js
@@ -1,6 +1,6 @@
 import { legacy_createStore as createStore } from "redux";
 
-import rootReducer from "./reducers";
+import rootReducer from "./reducer";
 
 const store = createStore(rootReducer);
 
