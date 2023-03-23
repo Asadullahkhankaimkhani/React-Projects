4afc29dd59d8365b74af732f40ae49df4329b164
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2023-03-24 00:36:55 +0500

    Configure redux devtools

--- redux/Starter-Project/src/index.js
+++ redux/Starter-Project/src/index.js
@@ -4,8 +4,8 @@ import { addTask, removeTask, updateTask, fetchTodos } from "./store/task";
 store.dispatch(addTask("Learn Redux"));
 console.log(store.getState());
 
-store.subscribe(() => {
-	console.log("Store changed!", store.getState());
+const unsubscribe = store.subscribe(() => {
+	console.log("Updated", store.getState());
 });
 
 store.dispatch(addTask("Learn React"));
@@ -13,4 +13,5 @@ store.dispatch(addTask("Learn React"));
 store.dispatch(updateTask(1));
 
 store.dispatch(fetchTodos());
+unsubscribe();
 console.log(store.getState());
--- redux/Starter-Project/src/store/storeConfigure.js
+++ redux/Starter-Project/src/store/storeConfigure.js
@@ -3,6 +3,9 @@ import thunk from "redux-thunk";
 
 import rootReducer from "./task";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+	rootReducer,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
