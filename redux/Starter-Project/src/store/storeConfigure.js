1fa4a57dc2a4d2852ae6b1a6d8059b4c98cb0349
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2023-03-25 23:36:28 +0500

    create own middleware

--- redux/Starter-Project/src/store/storeConfigure.js
+++ redux/Starter-Project/src/store/storeConfigure.js
@@ -12,12 +12,14 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import employeeReducer from "./employee";
 import taskReducer from "./task";
+import log from "../../middleware/log";
 
 const store = configureStore({
 	reducer: {
 		task: taskReducer,
 		employee: employeeReducer,
 	},
+	middleware: (getDefaultMiddleware) => [...getDefaultMiddleware(), log],
 });
 
 export default store;
